59aa348c832710a09fa03e23d5a8ddea634bfb6e
Siarhei B <sergeybednyuk@gmail.com>
2025-11-13 13:58:57 +0100

    add base configuration

--- eCommerce.Api/Program.cs
+++ eCommerce.Api/Program.cs
@@ -1,3 +1,6 @@
+using eCommerce.Application;
+using eCommerce.infrastructure;
+
 namespace eCommerce.Api;
 
 public class Program
@@ -5,10 +8,25 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+        
+        //add services registration
+        builder.Services.AddInfrastructure(builder.Configuration);
+        builder.Services.AddApplication();
+        builder.Services.AddControllers();
+        
         var app = builder.Build();
 
-        app.MapGet("/", () => "Hello World!");
-
+        //Routing
+        app.UseRouting();
+        
+        //Auth
+        app.UseAuthentication();
+        app.UseAuthorization();
+        
+        //Controller routing
+        app.MapControllers();
+        
+        //Run the app
         app.Run();
     }
 }
