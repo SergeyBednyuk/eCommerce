d974965f57271c99b97dfe541fff1c593fc8d490
Siarhei B <sergeybednyuk@gmail.com>
2025-11-13 13:39:14 +0100

    Init eCommerce app

---
+++ eCommerce.infrastructure/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace eCommerce.infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        //TODO: add services to IoC container
+        
+        return services;
+    }
+}
