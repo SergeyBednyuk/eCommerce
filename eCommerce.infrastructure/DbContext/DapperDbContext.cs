6014588bb901b353fe3d651466ebddbe618bed68
Siarhei B <sergeybednyuk@gmail.com>
2025-12-01 18:23:50 +0100

    Update docker configuration

--- eCommerce.infrastructure/DbContext/DapperDbContext.cs
+++ eCommerce.infrastructure/DbContext/DapperDbContext.cs
@@ -4,15 +4,18 @@ using Npgsql;
 
 namespace eCommerce.infrastructure.DbContext;
 
-public class DapperDbContext 
+public class DapperDbContext
 {
     private readonly IConfiguration _configuration;
-    private readonly IDbConnection  _dbConnection;
-    
-    public DapperDbContext(IConfiguration  configuration)
+    private readonly IDbConnection _dbConnection;
+
+    public DapperDbContext(IConfiguration configuration)
     {
         _configuration = configuration;
-        string? connectionString = _configuration.GetConnectionString("PostgresConnection");
+        string connectionStringTemplate = _configuration.GetConnectionString("PostgresConnection")!;
+        string connectionString = connectionStringTemplate
+            .Replace("$POSTGRES_HOST", Environment.GetEnvironmentVariable("POSTGRES_HOST") ?? "localhost")
+            .Replace("$POSTGRES_PASSWORD", Environment.GetEnvironmentVariable("POSTGRES_PASSWORD") ?? "postgres");
 
         _dbConnection = new NpgsqlConnection(connectionString);
     }
